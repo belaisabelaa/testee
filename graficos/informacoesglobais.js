ddf9f76cbb5c9840ee27bcec09fe1d94cae67efe
isabela <134521937+belaisabelaa@users.noreply.github.com>
2024-11-24 11:15:26 -0300

    .

---
+++ graficos/informacoesglobais.js
@@ -0,0 +1,12 @@
+const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
+
+async function visualizarinformacoesglobais() {
+ const res = await fetch(url);
+ const dados = res.json();
+ console.log(dados.tempo_medio);
+ const paragrafo = document.createElement('p')
+ paragrafo.classList.add('grafico-container_texto')
+ paragrafo.innerHTML = `você sabia que o mundo tem ${dados.total_pessoas_mundo'} de pessoas e aproximadamente &{dados.total_pessoas_conectadas} estão conectadas em alguma rede social e passam em média ${dados.tempo_medio}  horas conectadas.`
+console.log(paragrafo)
+}
+visualizarinformacoesglobais()
